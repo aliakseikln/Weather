521149299ab751be1cea68d74ae730139ff4a41f
aliakseikln <aliakseikln@gmail.com>
2022-09-10 22:23:44 +0300

    Added TabLayout and some changes in MainActivity and MainScreen

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.weather.screens.MainScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.weather.screens.MainCard
+import com.example.weather.screens.TabLayout
 import com.example.weather.ui.theme.WeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +19,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherTheme {
-                MainScreen()
+                Image(
+                    painter = painterResource(id = R.drawable.weather_background2),
+                    contentDescription = "Image main background",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.5f),
+                    contentScale = ContentScale.Crop
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
             }
         }
     }
--- app/src/main/java/com/example/weather/screens/MainScreen.kt
+++ app/src/main/java/com/example/weather/screens/MainScreen.kt
@@ -1,18 +1,14 @@
 package com.example.weather.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,105 +17,144 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.weather.R
 import com.example.weather.ui.theme.BlueLight
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Image(
-        painter = painterResource(id = R.drawable.weather_background2),
-        contentDescription = "Image Background",
-        modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.5f),
-        contentScale = ContentScale.Crop
-    )
+fun MainCard() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
             .padding(8.dp)
     ) {
         Card(
             modifier = Modifier.fillMaxWidth(),
             backgroundColor = BlueLight,
             elevation = 0.dp,
-            shape = RoundedCornerShape(10.dp)
+            shape = RoundedCornerShape(11.dp)
         ) {
             Column(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column(
+                Row(
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = "9 September 2022 15:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
+                        contentDescription = "Image mini current weather",
+                        modifier = Modifier
+                            .size(90.dp)
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "Minsk",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(
+                        onClick = {
+
+                        }
                     ) {
-                        Text(
-                            modifier = Modifier.padding(8.dp),
-                            text = "9 September 2022 15:00",
-                            style = TextStyle(fontSize = 15.sp),
-                            color = Color.White
-                        )
-                        AsyncImage(
-                            model = "https://cdn.weatherapi.com/weather/64x64/day/116.png",
-                            contentDescription = "Image mini current weather",
-                            modifier = Modifier
-                                .size(35.dp)
-                                .padding(top = 3.dp, end = 8.dp)
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "Image search",
+                            tint = Color.White
                         )
                     }
                     Text(
-                        text = "Minsk",
-                        style = TextStyle(fontSize = 24.sp),
-                        color = Color.White
-                    )
-                    Text(
-                        text = "23°C",
-                        style = TextStyle(fontSize = 65.sp),
-                        color = Color.White
-                    )
-                    Text(
-                        text = "Sunny",
+                        text = "23°C / 12°C",
                         style = TextStyle(fontSize = 16.sp),
                         color = Color.White
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                    IconButton(
+                        onClick = {
+
+                        }
                     ) {
-                        IconButton(
-                            onClick = {
-
-                            }
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_search),
-                                contentDescription = "Image search",
-                                tint = Color.White
-                            )
-                        }
-                        Text(
-                            text = "23°C / 12°C",
-                            style = TextStyle(fontSize = 16.sp),
-                            color = Color.White
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "Image sync",
+                            tint = Color.White
                         )
-                        IconButton(
-                            onClick = {
-
-                            }
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.ic_sync),
-                                contentDescription = "Image sync",
-                                tint = Color.White
-                            )
-                        }
-
                     }
+
                 }
             }
+
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("HOURS", "DAYS")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp)
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { pos ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, pos)
+                )
+            },
+            backgroundColor = BlueLight,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+                )
+            }
+        }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) { index ->
+
         }
     }
 }
